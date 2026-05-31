15aa97bc6745d960368cf501c54211905204f3d9
eugene <evgen.svatnenko@gmail.com>
2026-05-31 13:33:42 +0300

    Додав основу гри

--- eugene/1.py
+++ eugene/1.py
@@ -18,25 +18,6 @@ class Vikno:
         self.button=tk.Button(self.root,text="Грати",font=("Arial",20),command=self.pochatok)
         self.button.pack(pady=20)
 
-    """def knopa(self):
-        self.label.config(text="Treba rozpochaty game")
-
-        self.gaypole=tk.Toplevel(self.root)   #nashe pole dlia gri
-        self.gaypole.title("Gaypole")
-
-        self.gaypole.attributes("-fullscreen",True)
-
-        gay_label=tk.Label(self.gaypole,text="Megaladon",font=("Arial",20))
-        gay_label.place(relx=0.46,rely=0.05)     # koordinat ekranu u vidsotkax x=shirina y=visota
-
-        label1 = tk.Label(self.gaypole, text="Твоє поле", font=("Arial", 16), bg="lightblue")
-        label1.place(relx=0.15,rely=0.2)
-
-        label2 = tk.Label(self.gaypole, text="Поле робота", font=("Arial", 16), bg="yellow")
-        label2.place(relx=0.79,rely=0.2)
-
-        close_button=tk.Button(self.gaypole,text="zakrit igru",command=root.destroy)
-        close_button.place(relx=0.49,rely=0.9)"""
     def pochatok(self):
         self.label.config(text="Treba rozpochaty game")
 
@@ -50,25 +31,57 @@ class War:   #perepisav troshki kod tosho baran and robiv vse v odomu classi
         self.gaypole.title("Gaypole")
         self.gaypole.attributes("-fullscreen",True)
 
+        self.graves_data=[[0]*10 for i in range(10)]         #matruzi dla poliv
+        self.robot_data = [[0] * 10 for i in range(10)]
+
+        self.graves_buttons=[[None]*10 for i in range(10)]    #zapamiatovuvania knop
+        self.robot_buttons=[[None]*10 for i in range(10)]
+
         self.knopa()
 
     def knopa(self):
+
         gay_label = tk.Label(self.gaypole, text="Megaladon", font=("Arial", 20))
         gay_label.place(relx=0.46, rely=0.05)  # koordinat ekranu u vidsotkax x=shirina y=visota
-
+#pola gravsa
         label1 = tk.Label(self.gaypole, text="Твоє поле", font=("Arial", 16), bg="lightblue")
-        label1.place(relx=0.15, rely=0.2)
+        label1.place(relx=0.2, rely=0.1)
 
+        graves_frame = tk.Frame(self.gaypole, bg="lightblue")
+        graves_frame.place(relx=0.03, rely=0.2)
+        self.sitka(graves_frame, self.graves_buttons, isrb=False)
+
+
+#pole robota
         label2 = tk.Label(self.gaypole, text="Поле робота", font=("Arial", 16), bg="yellow")
-        label2.place(relx=0.79, rely=0.2)
+        label2.place(relx=0.7, rely=0.1)
 
-        close_button = tk.Button(self.gaypole, text="zakrit igru", command=root.destroy)
+        robot_frame = tk.Frame(self.gaypole, bg="lightblue")
+        robot_frame.place(relx=0.53, rely=0.2)
+        self.sitka(robot_frame, self.robot_buttons, isrb=True)
+
+        close_button = tk.Button(self.gaypole, text="zakrit igru", command=self.gaypole.destroy)
         close_button.place(relx=0.49, rely=0.9)
 
+    def sitka(self,frame,knopi_mat,isrb):
+        for r in range(10):
+            for c in range(10):   #lambda zamoroshey do momentu cliku
+                btn=tk.Button(
+                    frame,
+                    width=8,height=3,bg="white",
+                    command=lambda row=r,col=c,robot=isrb: self.nazat(row,col,robot) )
 
+                btn.grid(row=r,column=c,padx=1,pady=1)
 
+                knopi_mat[r][c]=btn
 
-
+    def nazat(self,row,col,robot):    #dla togo shob nazimat na polebou
+        if not robot:
+            self.graves_buttons[row][col].config(bg="gray")
+            self.graves_data[row][col]=1 # tut korablik
+        else:
+            self.robot_buttons[row][col].config(bg="Red",text="X")
+            self.robot_data[row][col]=3 #pidbili
 
 
 if __name__=="__main__":
