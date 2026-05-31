7db54ac85482dbe09472689d732068346d540a00
eugene <evgen.svatnenko@gmail.com>
2026-05-31 11:54:56 +0300

    Змінив друге вікно,створив другий клас

--- eugene/1.py
+++ eugene/1.py
@@ -15,10 +15,10 @@ class Vikno:
         self.label=tk.Label(self.root,text="Sea Battleship",font=("Arial",20)) #zrobili text
         self.label.pack(pady=20)
 
-        self.button=tk.Button(self.root,text="Грати",font=("Arial",20),command=self.knopa)
+        self.button=tk.Button(self.root,text="Грати",font=("Arial",20),command=self.pochatok)
         self.button.pack(pady=20)
 
-    def knopa(self):
+    """def knopa(self):
         self.label.config(text="Treba rozpochaty game")
 
         self.gaypole=tk.Toplevel(self.root)   #nashe pole dlia gri
@@ -27,12 +27,43 @@ class Vikno:
         self.gaypole.attributes("-fullscreen",True)
 
         gay_label=tk.Label(self.gaypole,text="Megaladon",font=("Arial",20))
-        gay_label.pack(pady=20)
+        gay_label.place(relx=0.46,rely=0.05)     # koordinat ekranu u vidsotkax x=shirina y=visota
+
+        label1 = tk.Label(self.gaypole, text="Твоє поле", font=("Arial", 16), bg="lightblue")
+        label1.place(relx=0.15,rely=0.2)
+
+        label2 = tk.Label(self.gaypole, text="Поле робота", font=("Arial", 16), bg="yellow")
+        label2.place(relx=0.79,rely=0.2)
 
         close_button=tk.Button(self.gaypole,text="zakrit igru",command=root.destroy)
-        close_button.pack(pady=20)
+        close_button.place(relx=0.49,rely=0.9)"""
+    def pochatok(self):
+        self.label.config(text="Treba rozpochaty game")
+
+        self.see=War(self.root)
 
 
+class War:   #perepisav troshki kod tosho baran and robiv vse v odomu classi
+    def __init__(self,root):
+        self.root=root
+        self.gaypole=tk.Toplevel(self.root)
+        self.gaypole.title("Gaypole")
+        self.gaypole.attributes("-fullscreen",True)
+
+        self.knopa()
+
+    def knopa(self):
+        gay_label = tk.Label(self.gaypole, text="Megaladon", font=("Arial", 20))
+        gay_label.place(relx=0.46, rely=0.05)  # koordinat ekranu u vidsotkax x=shirina y=visota
+
+        label1 = tk.Label(self.gaypole, text="Твоє поле", font=("Arial", 16), bg="lightblue")
+        label1.place(relx=0.15, rely=0.2)
+
+        label2 = tk.Label(self.gaypole, text="Поле робота", font=("Arial", 16), bg="yellow")
+        label2.place(relx=0.79, rely=0.2)
+
+        close_button = tk.Button(self.gaypole, text="zakrit igru", command=root.destroy)
+        close_button.place(relx=0.49, rely=0.9)
 
 
 
